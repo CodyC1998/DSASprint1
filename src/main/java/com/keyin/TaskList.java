801bfe0b25b55c0e7760ca646c1d7ce61c3ab639
CodyC1998 <cody.collins@keyin.com>
2025-06-26 12:51:13 -0230

    Added TaskList class with methods to add, complete, and print tasks

--- src/main/java/com/keyin/TaskList.java
+++ src/main/java/com/keyin/TaskList.java
@@ -1,4 +1,46 @@
 package com.keyin;
 
 public class TaskList {
+    private static class Node {
+        Task task;
+        Node next;
+
+        Node(Task task) {
+            this.task = task;
+        }
+    }
+
+    private Node head;
+
+    public void add(String description) {
+        Task task = new Task(description);
+        Node newNode = new Node(task);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) current = current.next;
+            current.next = newNode;
+        }
+    }
+
+    public void markCompleted(int index) {
+        Node current = head;
+        int count = 0;
+        while (current != null && count < index) {
+            current = current.next;
+            count++;
+        }
+        if (current != null) current.task.markAsCompleted();
+    }
+
+    public void printAll() {
+        Node current = head;
+        int index = 0;
+        while (current != null) {
+            System.out.println(index + ": " + current.task);
+            current = current.next;
+            index++;
+        }
+    }
 }
