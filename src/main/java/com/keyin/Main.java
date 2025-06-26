d1647327c5f967f15fa1e5b67a93cab246323466
CodyC1998 <cody.collins@keyin.com>
2025-06-26 18:54:47 -0230

    Added User.java class and updated Main to create users add tasks and mark them as complete and print list of tasks for each user

--- src/main/java/com/keyin/Main.java
+++ src/main/java/com/keyin/Main.java
@@ -2,6 +2,21 @@ package com.keyin;
 
 public class Main {
     public static void main(String[] args) {
+        User[] users = new User[2]; // change size as needed
 
+        users[0] = new User("Alice");
+        users[1] = new User("Bob");
+
+        users[0].addTask("Finish assignment");
+        users[0].addTask("Buy groceries");
+
+        users[1].addTask("Walk the dog");
+
+        users[0].markTaskAsCompleted(0);
+
+        for (User user : users) {
+            user.printTasks();
+            System.out.println();
         }
+    }
 }
--- src/main/java/com/keyin/User.java
+++ src/main/java/com/keyin/User.java
@@ -1,4 +1,28 @@
 package com.keyin;
 
 public class User {
+    private final String name;
+    private final TaskList taskList;
+
+    public User(String name) {
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addTask(String description) {
+        taskList.add(description);
+    }
+
+    public void markTaskAsCompleted(int index) {
+        taskList.markCompleted(index);
+    }
+
+    public void printTasks() {
+        System.out.println("Tasks for " + name + ":");
+        taskList.printAll();
+    }
 }
