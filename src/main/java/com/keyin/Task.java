7affb755c1da35aaf65ddd93d050d3dd1cc049fa
CodyC1998 <cody.collins@keyin.com>
2025-06-26 12:31:13 -0230

    added description and isCompleted to Task.java as well as method to mark a task as completed

--- src/main/java/com/keyin/Task.java
+++ src/main/java/com/keyin/Task.java
@@ -1,4 +1,19 @@
 package com.keyin;
 
 public class Task {
+    private final String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public void markAsCompleted() {
+        isCompleted = true;
+    }
+
+    public String toString() {
+        return description + " [" + (isCompleted ? "Completed" : "Pending") + "]";
+    }
 }
